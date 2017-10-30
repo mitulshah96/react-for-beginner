23550ffbbbf8d1c66d8fb1afcb3178d20baf7216
mitulshah96 <mitulshah96@gmail.com>
2017-10-30 16:23:24 +0530

    optimize state property

--- react-example.js
+++ react-example.js
@@ -1,3 +1,5 @@
+//example of function component and function component cannot have state
+//component properties are always immutable
 const Button=function(props){
 	return(
   //props is an object that hold all the value that were passed when component was rendered
@@ -6,5 +8,28 @@ const Button=function(props){
 	//React.createElement("button","null","GO")
 	)	;
 }
+
+//so lets upgrade this component to class component
+//class component
+//class component
+class Button extends React.Component{
+//initialize state object in button constructor class
+	state={counter:0}
+	handleClick=()=>{
+  // if you update state based on current state then use 2nd syntax of set state
+  	this.setState((prevState)=>({
+    counter:prevState.counter+1
+    }));
+  };
+	render(){
+  	return(
+    <button onClick={this.handleClick}>
+    {this.state.counter}
+    </button>
+    );
+  }
+}
+
+
 
 ReactDOM.render(<Button label="save"/>,mountNode);
